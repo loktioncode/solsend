6bd63fb4db44c083fa43db8c3b0c0e8f8fbaa1e1
elisha_b <elisha.b@cestasoft.co.za>
2022-02-12 17:19:57 +0200

    first commit

---
+++ components/ConnectToPhantom.tsx
@@ -0,0 +1,157 @@
+import { useEffect, useState } from "react";
+import { PublicKey, Transaction } from "@solana/web3.js";
+
+
+
+type DisplayEncoding = "utf8" | "hex";
+type PhantomEvent = "disconnect" | "connect" | "accountChanged";
+type PhantomRequestMethod =
+  | "connect"
+  | "disconnect"
+  | "signTransaction"
+  | "signAllTransactions"
+  | "signMessage";
+
+interface ConnectOpts {
+  onlyIfTrusted: boolean;
+}
+
+interface PhantomProvider {
+  publicKey: PublicKey | null;
+  isConnected: boolean | null;
+  signTransaction: (transaction: Transaction) => Promise<Transaction>;
+  signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>;
+  signMessage: (
+    message: Uint8Array | string,
+    display?: DisplayEncoding
+  ) => Promise<any>;
+  connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>;
+  disconnect: () => Promise<void>;
+  on: (event: PhantomEvent, handler: (args: any) => void) => void;
+  request: (method: PhantomRequestMethod, params: any) => Promise<unknown>;
+}
+
+function ConnectToPhantom() {
+  const [provider, setProvider] = useState<PhantomProvider | undefined>(
+    undefined
+  );
+  const [walletKey, setWalletKey] = useState<PhantomProvider | undefined>(
+    undefined
+  );
+
+  /**
+   * @description gets Phantom provider, if it exists
+   */
+  const getProvider = (): PhantomProvider | undefined => {
+    if ("solana" in window) {
+      // @ts-ignore
+      const provider = window.solana as any;
+      if (provider.isPhantom) return provider as PhantomProvider;
+    }
+  };
+
+  /**
+   * @description prompts user to connect wallet if it exists
+   */
+  const connectWallet = async () => {
+    // @ts-ignore
+    const { solana } = window;
+
+    if (solana) {
+      try {
+        const response = await solana.connect();
+        console.log("wallet account ", response.publicKey.toString());
+        setWalletKey(response.publicKey.toString());
+      } catch (err) {
+        // { code: 4001, message: 'User rejected the request.' }
+      }
+    }
+  };
+
+  /**
+   * @description disconnect Phantom wallet
+   */
+  const disconnectWallet = async () => {
+    // @ts-ignore
+    const { solana } = window;
+
+    if (walletKey && solana) {
+      await (solana as PhantomProvider).disconnect();
+      setWalletKey(undefined);
+    }
+  };
+
+  // detect phantom provider exists
+  useEffect(() => {
+    const provider = getProvider();
+
+    if (provider) setProvider(provider);
+    else setProvider(undefined);
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {provider && !walletKey && (
+          <button
+            className="button"
+            onClick={connectWallet}
+          >
+            Connect to Phantom Wallet
+          </button>
+        )}
+
+        {provider && walletKey && (
+          <div>
+            <p>Connected account {walletKey}</p>
+
+            <button
+              className="button"
+              onClick={disconnectWallet}
+            >
+              Disconnect
+            </button>
+          </div>
+        )}
+
+        {!provider && (
+          <p>
+            No provider found. Install{" "}
+            <a href="https://phantom.app/">Phantom Browser extension</a>
+          </p>
+        )}
+
+
+      </header>
+      <style jsx>{`
+.button{
+  background: linear-gradient(
+135deg
+, #753370 0%, #298096 100%);
+  padding: 15px;
+  border: none;
+  border-radius: 50px;
+  color: white;
+  font-weight: 400;
+  font-size: 1.2rem;
+  margin-top: 10px;
+  width:100%;
+  letter-spacing: .11rem;
+  outline:none;
+}
+
+.button:hover
+{
+transform: scale(1.05) translateY(-3px);
+  box-shadow: 3px 3px 6px #38373785;
+}
+
+
+      `}</style>
+
+
+    </div>
+  );
+}
+
+export default ConnectToPhantom;
---
+++ pages/index.jsx
@@ -0,0 +1,488 @@
+import Head from 'next/head'
+import { useState, useEffect } from "react";
+import * as web3 from "@solana/web3.js";
+import * as splToken from "@solana/spl-token";
+import ConnectToPhantom from "../components/ConnectToPhantom.tsx";
+
+import {
+  useCSVReader,
+  lightenDarkenColor,
+  formatFileSize,
+} from 'react-papaparse';
+
+const GREY = '#CCC';
+const GREY_LIGHT = 'rgba(255, 255, 255, 0.4)';
+const DEFAULT_REMOVE_HOVER_COLOR = '#A01919';
+const REMOVE_HOVER_COLOR_LIGHT = lightenDarkenColor(
+  DEFAULT_REMOVE_HOVER_COLOR,
+  40
+);
+const GREY_DIM = '#686868';
+
+const styles = {
+  zone: {
+    alignItems: 'center',
+    border: `2px dashed ${GREY}`,
+    borderRadius: 20,
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    justifyContent: 'center',
+    padding: 20,
+    cursor: 'pointer',
+  },
+  file: {
+    background: 'linear-gradient(to bottom, #EEE, #DDD)',
+    borderRadius: 20,
+    display: 'flex',
+    height: 120,
+    width: 120,
+    position: 'relative',
+    zIndex: 10,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  info: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  size: {
+    backgroundColor: GREY_LIGHT,
+    borderRadius: 3,
+    marginBottom: '0.5em',
+    justifyContent: 'center',
+    display: 'flex',
+  },
+  name: {
+    backgroundColor: GREY_LIGHT,
+    borderRadius: 3,
+    fontSize: 12,
+    marginBottom: '0.5em',
+  },
+  progressBar: {
+    bottom: 14,
+    position: 'absolute',
+    width: '100%',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  zoneHover: {
+    borderColor: GREY_DIM,
+  },
+  default: {
+    borderColor: GREY,
+  },
+  remove: {
+    height: 23,
+    position: 'absolute',
+    right: 6,
+    top: 6,
+    width: 23,
+  },
+};
+
+
+export default function Home() {
+
+  const [tokenAccountAddress, setTokenAccountAddress] = useState("");
+  const [tokenPrivateKey, setTokenPrivateKey] = useState("");
+  const [network, setNetwork] = useState("");
+
+  const { CSVReader } = useCSVReader();
+  const [zoneHover, setZoneHover] = useState(false);
+  const [removeHoverColor, setRemoveHoverColor] = useState(
+    DEFAULT_REMOVE_HOVER_COLOR
+  );
+
+  var whitelistAccount = {}, whiteListedAccounts = []
+
+  const getProvider = async () => {
+    if ("solana" in window) {
+      // opens wallet to connect to
+      await window.solana.connect();
+
+      const provider = window.solana;
+      if (provider.isPhantom) {
+        console.log("Is Phantom installed?  ", provider.isPhantom);
+    
+        return provider;
+      }
+    } else {
+      window.open("https://www.phantom.app/", "_blank");
+    }
+  };
+
+  useEffect(() => {
+    getProvider();
+
+  }, [])
+
+
+  async function handleTokenTransaction() {
+
+    var provider = await getProvider();
+    console.log("Public key of the emitter: ", provider.publicKey.toString());
+
+
+    const MY_SECRET_KEY = tokenPrivateKey.split(",")
+
+    const connection = new web3.Connection(
+      web3.clusterApiUrl(network),
+      'confirmed',
+    );
+
+    const fromWallet = web3.Keypair.fromSecretKey(
+      new Uint8Array(MY_SECRET_KEY)
+    )
+
+    //to wallet
+    let tokenRecieverPubkey
+
+    // Construct my token class
+    var tokenAccount = new web3.PublicKey(tokenAccountAddress);
+    var myToken = new splToken.Token(
+      connection,
+      tokenAccount,
+      splToken.TOKEN_PROGRAM_ID,
+      fromWallet
+    );
+
+
+    for (let i = 0; i < whiteListedAccounts.length; i++) {
+      tokenRecieverPubkey = new web3.PublicKey(whiteListedAccounts[i].address)
+
+      // Create associated token accounts for my token if they don't exist yet
+      var fromTokenAccount = await myToken.getOrCreateAssociatedAccountInfo(
+        fromWallet.publicKey
+      )
+      var toTokenAccount = await myToken.getOrCreateAssociatedAccountInfo(
+        tokenRecieverPubkey
+      )
+
+      // Add token transfer instructions to transaction
+      var transferTokens = new web3.Transaction().add(
+        splToken.Token.createTransferInstruction(
+          splToken.TOKEN_PROGRAM_ID,
+          fromTokenAccount.address,
+          toTokenAccount.address,
+          fromWallet.publicKey,
+          [],
+          whiteListedAccounts[i].tokens
+        )
+      );         
+        
+      // const signature = await web3.sendAndConfirmTransaction(
+      //   connection,
+      //   transferTokens,
+      //   [fromWallet],
+      //   { commitment: 'confirmed' },
+      // );
+
+      console.log("Transaction confirmed with signature:", signature)
+      console.log("MA TOKEN ABAYA!")
+    }
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleTokenTransaction()
+
+  }
+
+
+
+  return (
+    <div className="container">
+      <Head>
+        <title>SendTokens</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div className="mainscreen">
+          {/* <img src="https://image.freepik.com/free-vector/purple-background-with-neon-frame_52683-34124.jpg" className="bgimg " alt=""> */}
+          <div className="card">
+            <div className="leftside">
+              <img
+                src="https://solana.com/src/img/branding/solanaLogoMark.svg"
+                className="product"
+                alt="solana-logo"
+              />
+            </div>
+            <div className="rightside">
+              <form action="" onSubmit={handleSubmit}>
+                <h1>Distribute SPL tokens</h1>
+                <h2>Tranfer info</h2>
+
+                <p>TOKEN ACCOUNT ADDRESS</p>
+                <input type="text" className="inputbox" onChange={(e) => setTokenAccountAddress(e.target.value)} required />
+                <p>PRIVATE KEY</p>
+                <input type="password" className="inputbox" onChange={(e) => setTokenPrivateKey(e.target.value)} required />
+
+                <p>NETWORK/CHAIN</p>
+                <select className="inputbox" onChange={(e) => setNetwork(e.target.value)} required>
+                  <option value="">--Select a Chain--</option>
+                  <option value="devnet">DevNet</option>
+                  <option value="mainnet">MainNet</option>
+                </select>
+                <div className="expcvv">
+
+
+                  <CSVReader
+                    onUploadAccepted={(results) => {
+
+                      for (let index = 1; index < results.data.length; index++) {
+                        const element = results.data[index];
+
+                        if (element[0] !== null && element[0] !== '' && element[0] != 'undefined') {
+                          // console.log(element[0]);
+                          // console.log(element[1])
+                          whitelistAccount.address = element[0];
+                          whitelistAccount.tokens = element[1];
+                          whiteListedAccounts.push(whitelistAccount);
+                        }
+
+                      }
+                      console.log('---------------------------');
+                      console.log(whiteListedAccounts);
+                      console.log('---------------------------');
+
+                      setZoneHover(false);
+                    }}
+                    onDragOver={(event) => {
+                      event.preventDefault();
+                      setZoneHover(true);
+                    }}
+                    onDragLeave={(event) => {
+                      event.preventDefault();
+                      setZoneHover(false);
+                    }}
+                  >
+                    {({
+                      getRootProps,
+                      acceptedFile,
+                      ProgressBar,
+                      getRemoveFileProps,
+                      Remove,
+                    }) => (
+                      <>
+                        <div
+                          {...getRootProps()}
+                          style={Object.assign(
+                            {},
+                            styles.zone,
+                            zoneHover && styles.zoneHover
+                          )}
+                        >
+                          {acceptedFile ? (
+                            <>
+                              <div style={styles.file}>
+                                <div style={styles.info}>
+                                  <span style={styles.size}>
+                                    {formatFileSize(acceptedFile.size)}
+                                  </span>
+                                  <span style={styles.name}>{acceptedFile.name}</span>
+                                </div>
+                                <div style={styles.progressBar}>
+                                  <ProgressBar />
+                                </div>
+                                <div
+                                  {...getRemoveFileProps()}
+                                  style={styles.remove}
+                                  onMouseOver={(event) => {
+                                    event.preventDefault();
+                                    setRemoveHoverColor(REMOVE_HOVER_COLOR_LIGHT);
+                                  }}
+                                  onMouseOut={(event) => {
+                                    event.preventDefault();
+                                    setRemoveHoverColor(DEFAULT_REMOVE_HOVER_COLOR);
+                                  }}
+                                >
+                                  <Remove color={removeHoverColor} />
+                                </div>
+                              </div>
+                            </>
+                          ) : (
+                            'Drop Whitelist CSV file here or click to upload'
+                          )}
+                        </div>
+                      </>
+                    )}
+                  </CSVReader>
+                </div>
+                <p></p>
+
+        
+              <button type="submit" className="button">Transfer Tokens</button>
+        
+
+              </form>
+
+              {/* <ConnectToPhantom /> */}
+
+            </div>
+          </div>
+          <a
+            href="https://twitter.com/loktioncode"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: 'white' }}
+          >
+            Powered by{' '}
+            @loktioncode
+          </a>
+        </div>
+
+
+      </main>
+
+      <style jsx>{`
+
+
+.mainscreen
+{
+min-height: 100vh;
+width: 100%;
+display: flex;
+  flex-direction: column;
+  background-color: #DFDBE5;
+  background-image: url("https://wallpaperaccess.com/full/3063067.png");
+  color:#963E7B;
+}
+
+.card {
+width: 60rem;
+  margin: auto;
+  background: white;
+  position:center;
+  align-self: center;
+  top: 50rem;
+  border-radius: 1.5rem;
+  box-shadow: 4px 3px 20px #3535358c;
+  display:flex;
+  flex-direction: row;
+  
+}
+
+.leftside {
+background: #030303;
+width: 25rem;
+display: inline-flex;
+  align-items: center;
+  justify-content: center;
+border-top-left-radius: 1.5rem;
+  border-bottom-left-radius: 1.5rem;
+}
+
+.product {
+  object-fit: cover;
+width: 10em;
+  height: 10em;
+  border-radius: 100%;
+}
+
+.rightside {
+  background-color: #ffffff;
+width: 35rem;
+border-bottom-right-radius: 1.5rem;
+  border-top-right-radius: 1.5rem;
+  padding: 1rem 2rem 3rem 3rem;
+}
+
+p{
+  display:block;
+  font-size: 1.1rem;
+  font-weight: 400;
+  margin: .8rem 0;
+}
+
+.inputbox
+{
+  color:#030303;
+width: 100%;
+  padding: 0.5rem;
+  border: none;
+  border-bottom: 1.5px solid #ccc;
+  margin-bottom: 1rem;
+  border-radius: 0.3rem;
+  font-family: 'Roboto', sans-serif;
+  color: #615a5a;
+  font-size: 1.1rem;
+  font-weight: 500;
+outline:none;
+}
+
+.expcvv {
+  display:flex;
+  justify-content: space-between;
+  padding-top: 0.6rem;
+  justify-content:center;
+}
+
+.expcvv_text{
+  padding-right: 0.5rem;
+  width: 250px;
+}
+.expcvv_text2{
+  padding:0 1rem;
+}
+
+.button{
+  background: linear-gradient(
+135deg
+, #753370 0%, #298096 100%);
+  padding: 15px;
+  border: none;
+  border-radius: 50px;
+  color: white;
+  font-weight: 400;
+  font-size: 1.2rem;
+  margin-top: 10px;
+  width:100%;
+  letter-spacing: .11rem;
+  outline:none;
+}
+
+.button:hover
+{
+transform: scale(1.05) translateY(-3px);
+  box-shadow: 3px 3px 6px #38373785;
+}
+
+@media only screen and (max-width: 1000px) {
+  .card{
+      flex-direction: column;
+      width: auto;  
+  }
+  .leftside{
+      width: 100%;
+      border-top-right-radius: 0;
+      border-bottom-left-radius: 0;
+    border-top-right-radius:0;
+    border-radius:0;
+  }
+
+  .rightside{
+      width:auto;
+      border-bottom-left-radius: 1.5rem;
+      padding:0.5rem 3rem 3rem 2rem;
+    border-radius:0;
+  }
+}
+      `}</style>
+
+      <style jsx global>{`
+    body {
+      font-family: 'Roboto', sans-serif!important;
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+    }
+      `}</style>
+    </div>
+  )
+}
